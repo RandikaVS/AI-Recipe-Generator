bbd7c92b36ac708429dfe4d91bad29eb3fe852d2
RandikaVS <randikasahan652@gmail.com>
2025-08-06 17:21:27 +0530

    add home page recent search result show

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,13 @@ import "../styles/Home.css"
 import IngredientInput from "../components/IngredientInput"
 import { MainContext } from "../context/main/main-context"
 import RecipeCard from "../components/RecipeCard"
+import { useLocalStorage } from "../hooks/use-local-storage"
 
 function Home() {
 
   const { getRecipeFromOpenAI, recipes } = useContext(MainContext)
+
+  const { state: recentRecipes, update: updateRecentRecipes } = useLocalStorage('recent_recipes', []);
 
   const [listRecipes, setListRecipes] = React.useState([])
   const [newRecipes, setNewRecipes] = React.useState([])
@@ -39,6 +42,7 @@ function Home() {
     
     if(recipes && recipes.length > 0) {
       setSearchResult(recipes)
+      updateRecentRecipes('recent', recipes);
     }
 
   }, [recipes])
@@ -146,7 +150,9 @@ function Home() {
                 </div>
               </div>
             </div>
+
           </div>
+
           <div className="col text-center text-lg-end animate__animated animate__fadeInRight">
             <img
               src="./img/Rectangle-313.webp"
@@ -157,136 +163,133 @@ function Home() {
         </div>
       </div>
 
-      <div
-        className="container d-flex align-items-center mt-3 mb-5 animate__animated animate__flipInX"
-        style={{ height: "80px" }}
-      >
-        <div
-          className="vr"
-          style={{ width: "15px", backgroundColor: "#efc81a", opacity: "100%" }}
-        ></div>
-        <p className="m-0 ms-3 fs-1 fw-semibold" style={{ color: "#3f3a3a" }}>
-          Popular For You!
-        </p>
-      </div>
-
-      <div className="container" style={{ marginBottom: "100px" }}>
-        <div className="row flex-column gap-5 flex-lg-row py-5">
-          <div className="col text-center text-lg-start animate__animated animate__fadeInLeft">
-            <img
-              src="./img/nasi-goreng-sederhana.webp"
-              alt="food"
-              style={{ width: "80%" }}
-            />
-          </div>
-          <div className="col-8 col-lg-4 d-flex flex-column d-lg-block justify-content-center align-self-center animate__animated animate__fadeInRight">
-            <h2
-              className="text-center text-lg-start fs-1"
-              style={{ color: "#3f3a3a" }}
-            >
-              Nasi Goreng Sederhana
-            </h2>
-            <hr className="opacity-100" />
-            <p
-              style={{ color: "#3f3a3a" }}
-              className="text-center text-lg-start"
-            >
-              Resep Nasi Goreng Sederhana, Praktis Lezat Hanya dengan Lima Bahan
+      {recentRecipes?.recent?.length > 0 ? (
+        <>
+          <div
+            className="container d-flex align-items-center mt-3 mb-5 animate__animated animate__flipInX"
+            style={{ height: "80px" }}
+          >
+            <div
+              className="vr"
+              style={{ width: "15px", backgroundColor: "#efc81a", opacity: "100%" }}
+            ></div>
+            <p className="m-0 ms-3 fs-1 fw-semibold" style={{ color: "#3f3a3a" }}>
+              Your Recent Searches
             </p>
-            <Link
-              to="/detail-recipe/12"
-              className="btn btn-lg"
-              style={{ backgroundColor: "#efc81a", color: "#fff" }}
-            >
-              Learn More
-            </Link>
           </div>
-        </div>
-      </div>
 
-      <div
-        className="container d-flex align-items-center my-5 animate__animated animate__flipInX"
-        style={{ height: "80px" }}
-      >
+          <div className="container px-4 px-md-4 py-5 mb-5 container-popular-recipe">
+            <div className="row justify-content-center gap-1 gap-sm-2 gap-md-4">
+              {recentRecipes.recent.map((item, index) => (
+                <RecipeCard recipe={item} key={index} />
+              ))}
+            </div>
+          </div>
+        </>
+    ):(
+
+      <>
+
         <div
-          className="vr"
-          style={{ width: "15px", backgroundColor: "#efc81a", opacity: "100%" }}
-        ></div>
-        <p className="m-0 ms-3 fs-1 fw-semibold" style={{ color: "#3f3a3a" }}>
-          New Recipe
-        </p>
-      </div>
-
-      <div className="left-layer animate__animated animate__fadeIn"></div>
-
-      <div className="container" style={{ marginBottom: "150px" }}>
-        <div className="row flex-column gap-5 flex-lg-row py-5">
-          <div className="col text-center text-lg-start animate__animated animate__fadeInLeft">
-            <img
-              src='./img/Rectangle-319.webp'
-              alt="food"
-              onError={addDefaultSrc}
-              style={{ width: "80%" }}
-            />
-          </div>
-          <div className="col-8 col-lg-4 d-flex flex-column d-lg-block justify-content-center align-self-center animate__animated animate__fadeInRight">
-            <h2
-              className="text-center text-lg-start fs-1"
-              style={{ color: "#3f3a3a" }}
-            >
-              {newRecipes?.title}
-            </h2>
-            <hr className="opacity-100" style={{ width: "25% !important" }} />
-            <p
-              style={{ color: "#3f3a3a" }}
-              className="text-center text-lg-start"
-            >
-              Resep Terbaru yang Dapat Anda coba untuk Keluarga Kesayangan
-            </p>
-            <Link
-              to={`/detail-recipe/${newRecipes?.id}`}
-              className="btn btn-lg"
-              style={{ backgroundColor: "#efc81a", color: "#fff" }}
-            >
-              Learn More
-            </Link>
-          </div>
+          className="container d-flex align-items-center mt-3 mb-5 animate__animated animate__flipInX"
+          style={{ height: "80px" }}
+        >
+          <div
+            className="vr"
+            style={{ width: "15px", backgroundColor: "#efc81a", opacity: "100%" }}
+          ></div>
+          <p className="m-0 ms-3 fs-1 fw-semibold" style={{ color: "#3f3a3a" }}>
+            Popular For You!
+          </p>
         </div>
-      </div>
 
-      <div
-        className="container d-flex align-items-center my-5 animate__animated animate__flipInX"
-        style={{ height: "80px" }}
-      >
-        <div
-          className="vr"
-          style={{ width: "15px", backgroundColor: "#efc81a", opacity: "100%" }}
-        ></div>
-        <p className="m-0 ms-3 fs-1 fw-semibold" style={{ color: "#3f3a3a" }}>
-          All Recipe
-        </p>
-      </div>
-
-      <div
-        className="container px-4 px-md-4 py-5 mb-5 container-popular-recipe"
-        id="popular-recipe"
-      >
-        <div className="row justify-content-center gap-1 gap-sm-2 gap-md-4">
-          {listRecipes.map((item, index) => {
-            return (
-              <RecipeCard
-                title={item?.title}
-                image={item?.recipePicture}
-                id={item?.id}
-                key={index}
+        <div className="container" style={{ marginBottom: "100px" }}>
+          <div className="row flex-column gap-5 flex-lg-row py-5">
+            <div className="col text-center text-lg-start animate__animated animate__fadeInLeft">
+              <img
+                src="./img/nasi-goreng-sederhana.webp"
+                alt="food"
+                style={{ width: "80%" }}
               />
-            )
-          })}
+            </div>
+            <div className="col-8 col-lg-4 d-flex flex-column d-lg-block justify-content-center align-self-center animate__animated animate__fadeInRight">
+              <h2
+                className="text-center text-lg-start fs-1"
+                style={{ color: "#3f3a3a" }}
+              >
+                Nasi Goreng Sederhana
+              </h2>
+              <hr className="opacity-100" />
+              <p
+                style={{ color: "#3f3a3a" }}
+                className="text-center text-lg-start"
+              >
+                Resep Nasi Goreng Sederhana, Praktis Lezat Hanya dengan Lima Bahan
+              </p>
+              <Link
+                to="/detail-recipe/12"
+                className="btn btn-lg"
+                style={{ backgroundColor: "#efc81a", color: "#fff" }}
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
 
-      {/* <Footer /> */}
-    </div>
+        <div
+          className="container d-flex align-items-center my-5 animate__animated animate__flipInX"
+          style={{ height: "80px" }}
+        >
+          <div
+            className="vr"
+            style={{ width: "15px", backgroundColor: "#efc81a", opacity: "100%" }}
+          ></div>
+          <p className="m-0 ms-3 fs-1 fw-semibold" style={{ color: "#3f3a3a" }}>
+            New Recipe
+          </p>
+        </div>
+
+        <div className="left-layer animate__animated animate__fadeIn"></div>
+
+        <div className="container" style={{ marginBottom: "150px" }}>
+          <div className="row flex-column gap-5 flex-lg-row py-5">
+            <div className="col text-center text-lg-start animate__animated animate__fadeInLeft">
+              <img
+                src='./img/Rectangle-319.webp'
+                alt="food"
+                onError={addDefaultSrc}
+                style={{ width: "80%" }}
+              />
+            </div>
+            <div className="col-8 col-lg-4 d-flex flex-column d-lg-block justify-content-center align-self-center animate__animated animate__fadeInRight">
+              <h2
+                className="text-center text-lg-start fs-1"
+                style={{ color: "#3f3a3a" }}
+              >
+                {newRecipes?.title}
+              </h2>
+              <hr className="opacity-100" style={{ width: "25% !important" }} />
+              <p
+                style={{ color: "#3f3a3a" }}
+                className="text-center text-lg-start"
+              >
+                Resep Terbaru yang Dapat Anda coba untuk Keluarga Kesayangan
+              </p>
+              <Link
+                to={`/detail-recipe/${newRecipes?.id}`}
+                className="btn btn-lg"
+                style={{ backgroundColor: "#efc81a", color: "#fff" }}
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
+        </div>
+
+       </>
+    )}
+  </div>
   )
 }
 
